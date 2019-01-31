7b884daa25f747d7e0e9aedd2ff2a2ea4e4e4947
Jesus <jesushc@unam.mx>
2019-01-31 16:17:10 -0600

    Codigo de la primer clase

---
+++ Clase1/src/clase1/Clase1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1;
+
+import is.ico.fes.aragon.unam.Automovil;
+/**
+ *
+ * @author Usuario-16
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Automovil auto1=new Automovil();
+        auto1.encender();
+        auto1.marca="Volks Wagen";
+        auto1.encender();
+    }
+    
+}
---
+++ Clase1/src/is/ico/fes/aragon/unam/Automovil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is.ico.fes.aragon.unam;
+
+/**
+ *
+ * @author Usuario-16
+ */
+public class Automovil {
+    public String marca;
+    public String subMarca;
+    public int modelo;
+    public String color;
+    
+    public Automovil(){
+        this.marca="Sin marca";
+        this.subMarca="Sin submarca";
+        this.modelo=0;
+        this.color="Sin color";
+    }
+    
+    public void encender(){
+        System.out.println("Encendiendo el " + this.marca);
+    }
+    
+    public void avanzar(){
+        System.out.println("Avanzando el " + this.marca);
+    }
+    
+    public void apagar(){
+        System.out.println("Apagando el "+ this.marca);
+    }
+}
