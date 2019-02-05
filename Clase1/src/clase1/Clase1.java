d405c816479468e944d4d7f355e8a556406f2048
AaronVelasco93 <aaronvelasco1993@outlook.com>
2019-02-05 16:40:01 -0600

    .

--- Clase1/src/clase1/Clase1.java
+++ Clase1/src/clase1/Clase1.java
@@ -16,10 +16,52 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Automovil auto1=new Automovil();
+      /*  Automovil auto1=new Automovil();
         auto1.encender();
         auto1.marca="Volks Wagen";
-        auto1.encender();
+        auto1.encender();*/
+        
+        //Bocho
+        Automovil miBocho = new Automovil();
+        miBocho.marca="VW";
+        miBocho.subMarca="Sedan";
+        miBocho.modelo=1970;
+        miBocho.color="Azul";
+       
+        //MiBocho Metodos
+        miBocho.encender();
+        miBocho.avanzar();
+        miBocho.apagar();
+        
+        System.out.println("-----------------------------");
+   
+        //Mustang
+        Automovil miMustang = new Automovil();
+        miBocho.marca="Ford";
+        miBocho.subMarca="Mustang";
+        miBocho.modelo=2010;
+        miBocho.color="Amarillo";
+        //Mustang
+        miMustang.encender();
+        miMustang.avanzar();
+        miMustang.apagar();
+
+        System.out.println("-----------------------------");
+
+        //Akura
+        Automovil miAkura = new Automovil();
+        miBocho.marca="Akura";
+        miBocho.subMarca="NSX";
+        miBocho.modelo=2013;
+        miBocho.color="Gris";
+       
+        
+        //Akura
+        miAkura.encender();
+        miAkura.avanzar();
+        miAkura.apagar();
+        
+        
     }
     
 }
